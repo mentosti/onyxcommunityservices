f099d73fdbe165c7e0a4f5a8098e691ae2a6e0a6
Dangthanhhuy Do <mentosti29071994@gmail.com>
2017-10-25 23:38:41 -0400

    test page

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ $(document).ready(function()
             $(".navbar-brand").css("margin", "20px 15px");
         }
     })
-    document.writeln("new: " + $(document).width());
+    document.writeln("new: " + $(window).width());
     if ($(window).width() <= 1024) {
         $(".dropdown").click(function(e) {
             e.preventDefault();
