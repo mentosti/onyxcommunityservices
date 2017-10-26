79ce53673f33b4f2dd4587a67de3dfc977de2561
Dangthanhhuy Do <mentosti29071994@gmail.com>
2017-10-25 23:09:02 -0400

    test page

---
+++ js/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function()
+{
+    // check scrollbar is not at top to minimize nav bar
+    $(window).scroll(function() {
+        if($(window).scrollTop() > 20) {
+            $(".navbar-fixed-top").height("75px");
+            $(".navbar-brand > img").height("55px");
+            $(".navbar-brand").css("margin", "10px 15px");
+        }
+        else {
+            $(".navbar-fixed-top").height("140px");
+            $(".navbar-brand > img").height("100px");
+            $(".navbar-brand").css("margin", "20px 15px");
+        }
+    })
+    
+    if ($(window).width() <= 1024) {
+        $(".dropdown").click(function(e) {
+            e.preventDefault();
+            
+            $(this).find(".dropdown-menu").first().toggle();
+        })
+        $(".dropdown>a").each(function() {
+            $(this).html($(this).text() + ' <span class="caret"></span>');
+        })
+    }
+    $(window).resize(function(){
+        $(".dropdown").off("click");
+        if ($(this).width() <= 1024) {
+            $(".dropdown").click(function(e) {
+                e.preventDefault();
+                $(this).find(".dropdown-menu").first().toggle();
+            })
+            $(".dropdown>a").each(function() {
+                $(this).html($(this).text() + ' <span class="caret"></span>');
+            })
+        }
+        else {
+            $(".dropdown>a").each(function() {
+                $(this).html($(this).text());
+            })
+        }
+    })
+    
+});
